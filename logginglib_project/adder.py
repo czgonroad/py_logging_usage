adcacc6522ad598cc2ab6e479bf65f0642ceda69
he.zhiming <he-zhiming@foxmail.com>
2018-06-14 23:36:36 +0800

    add class

---
+++ logginglib_project/adder.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# date:        2018/6/14
+# author:      he.zhiming
+#
+
+"""
+本文件描述:
+"""
+
+from __future__ import unicode_literals, absolute_import
+
+
+class Adder:
+    def add(self, a, b):
+        return a + b
